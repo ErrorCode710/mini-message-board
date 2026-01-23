ceb75a7db6b5f1e5b44f91de82f9dab7926f42ad
Error710 <jhunrey.canasa@ctu.edu.ph>
2026-01-24 00:26:00 +0800

    adding script on package.json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
 const readRouter = require("./routes/readRouter");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
